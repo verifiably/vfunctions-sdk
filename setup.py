1ed1ebce252b7f2b5abb67f96ee14e76428e3173
Santiago Passos <santiago.patino@verifiably.com>
2022-03-17 11:04:42 -0500

    Updated package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'A package to manage development inside nitro enclaves.'
 LONG_DESCRIPTION = long_description
 
